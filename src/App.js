f9349d8f87138cd3606ae73dad7c73e24a142cde
sana197111 <yc0047@gmail.com>
2024-02-02 14:51:25 +0900

    type

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { Introduction } from './components/Introduction';
 import { Register } from './components/Register';
 import { Page1 } from './components/Page1';
 import { Page2 } from './components/Page2';
+import { Page3 } from './components/Page3';
 import { Page2Test } from './components/Page2Test';
+import { Page3Test } from './components/Page3Test';
+import { Page3Result } from './components/Page3Result';
 import './App.css';
 
 function App() {
@@ -27,7 +30,10 @@ function AppRoutes() {
         <Route path="/register" element={<  Register />} />
         <Route path="/page1" element={<  Page1 />} />
         <Route path="/page2" element={<  Page2 />} />
+        <Route path="/page3" element={<  Page3 />} />
         <Route path="/page2test" element={<  Page2Test />} />
+        <Route path="/page3test" element={<  Page3Test />} />
+        <Route path="/page3result/:typeNumber" element={<Page3Result />} />
       </Routes>
   );
 }
